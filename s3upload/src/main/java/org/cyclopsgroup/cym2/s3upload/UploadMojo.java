3133767bf150b29ff89da56823e97f5f09e341a9
Jiaqi Guo <jguo@groupon.com>
2014-04-20 11:11:06 -0400

    UploadMojo that works

--- s3upload/src/main/java/org/cyclopsgroup/cym2/s3upload/UploadMojo.java
+++ s3upload/src/main/java/org/cyclopsgroup/cym2/s3upload/UploadMojo.java
@@ -3,6 +3,7 @@ package org.cyclopsgroup.cym2.s3upload;
 import java.io.File;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -14,7 +15,10 @@ import org.apache.maven.settings.Settings;
 import org.apache.maven.shared.model.fileset.FileSet;
 import org.apache.maven.shared.model.fileset.util.FileSetManager;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.internal.StaticCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 
@@ -34,6 +38,15 @@ public class UploadMojo
     @Parameter
     private String server;
 
+    @Parameter
+    private String awsAccessKeyId;
+
+    @Parameter
+    private String awsSecretKey;
+
+    @Parameter
+    private boolean instanceProfileUsed;
+
     /**
      * @inheritDoc
      */
@@ -41,17 +54,49 @@ public class UploadMojo
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
-        Server s = settings.getServer( server );
-        AmazonS3 s3 =
-            new AmazonS3Client( new BasicAWSCredentials( s.getUsername(),
-                                                         s.getPassword() ) );
+        AWSCredentialsProvider creds;
+        if ( StringUtils.isNotBlank( awsAccessKeyId )
+            && StringUtils.isNotBlank( awsSecretKey ) )
+        {
+            creds =
+                new StaticCredentialsProvider(
+                                               new BasicAWSCredentials(
+                                                                        awsAccessKeyId,
+                                                                        awsSecretKey ) );
+        }
+        else if ( instanceProfileUsed )
+        {
+            creds = new InstanceProfileCredentialsProvider();
+        }
+        else if ( StringUtils.isNotBlank( server ) )
+        {
+            Server s = settings.getServer( server );
+            creds =
+                new StaticCredentialsProvider(
+                                               new BasicAWSCredentials(
+                                                                        s.getUsername(),
+                                                                        s.getPassword() ) );
+        }
+        else
+        {
+            throw new MojoFailureException(
+                                            "Must specify awsAccessKeyId+awsSecretKey, server or instanceProfileUsed to provide AWS credentials" );
+        }
+        AmazonS3 s3 = new AmazonS3Client( creds );
         FileSetManager fileSetManager = new FileSetManager( getLog() );
         for ( FileSet fs : fileSets )
         {
-            String destPath = fs.getOutputDirectory();
+            String destPath = StringUtils.trimToEmpty( fs.getOutputDirectory() );
+            if ( StringUtils.isNotBlank( destPath ) && !destPath.endsWith( "/" ) )
+            {
+                destPath += "/";
+            }
             for ( String file : fileSetManager.getIncludedFiles( fs ) )
             {
                 File source = new File( fs.getDirectory(), file );
+
+                getLog().info( "Uploading file " + source + " to s3://"
+                                   + bucket + ":" + destPath + file );
                 s3.putObject( bucket, destPath + file, source );
             }
         }
