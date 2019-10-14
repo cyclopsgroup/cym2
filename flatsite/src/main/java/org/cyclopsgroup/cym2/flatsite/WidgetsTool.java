fecbf21d7c208c70e5a02ae6bdacd6bf8ba47ec3
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2019-10-13 17:41:08 -0700

    Remove experimental docs

--- flatsite/src/main/java/org/cyclopsgroup/cym2/flatsite/WidgetsTool.java
+++ flatsite/src/main/java/org/cyclopsgroup/cym2/flatsite/WidgetsTool.java
@@ -3,36 +3,24 @@ package org.cyclopsgroup.cym2.flatsite;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-
+import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.IOUtils;
 
-public class WidgetsTool
-{
-    public static final String TOOL_NAME = "widgets";
+public class WidgetsTool {
+  public static final String TOOL_NAME = "widgets";
 
-    public String renderHtmlBody( String url )
-        throws IOException
-    {
-        return renderHtmlBody( new URL( url ) );
-    }
+  public String renderHtmlBody(String url) throws IOException {
+    return renderHtmlBody(new URL(url));
+  }
 
-    public String renderHtmlBody( URL url )
-        throws IOException
-    {
-        InputStream in = url.openStream();
-        try
-        {
-            String content = IOUtils.toString( in );
-            if ( content.contains( "</body>" ) )
-            {
-                content = content.replaceAll( "^.*<body", "<div" );
-                content = content.replaceAll( "</body>.*$", "</div>" );
-            }
-            return content;
-        }
-        finally
-        {
-            IOUtils.closeQuietly( in );
-        }
+  public String renderHtmlBody(URL url) throws IOException {
+    try (InputStream in = url.openStream()) {
+      String content = IOUtils.toString(in, StandardCharsets.UTF_8);
+      if (content.contains("</body>")) {
+        content = content.replaceAll("^.*<body>", "<div>");
+        content = content.replaceAll("</body>.*$", "</div>");
+      }
+      return content;
     }
+  }
 }
